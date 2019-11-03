5718447e646ffd0f7026b62f2c71d1c4e1734ca5
nicky-code <aline.nicole7@gmail.com>
2019-11-04 00:32:25 +0200

    created the neighborhood function and where to display it as neighborhood.html file.

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -5,7 +5,20 @@ from .models import Neighborhood,Profile,Business
 # Create your views here.
 def welcome(request):
     
-    current_user=request.current_user
+    current_user = request.user
     neighbours = Neighborhood.objects.all()
     profile = Profile.objects.filter(user=current_user).first()
     return render(request, 'welcome.html',{'neighbours':neighbours,'profile':profile})
+
+
+def neighborhood(request,neighborhood_id):
+    
+    current_user = request.user
+    neighbours = Neighborhood.objects.get(id=neighborhood_id)
+    print(neighbours)
+    
+    profile=Profile.objects.filter(id=current_user.id).first()
+    businesses=Business.objects.filter(id=neighbours.id).all()
+    return render(request, 'neighborhood.html',{'businesses':businesses,'neighbours':neighbours,'neighborhood_id':neighborhood_id})
+
+
