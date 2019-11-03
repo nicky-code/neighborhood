2b26fda191d88b2576b48858d74319f83dffec16
nicky-code <aline.nicole7@gmail.com>
2019-11-03 22:39:52 +0200

    created the landing page.

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -3,4 +3,4 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Neighborhood')
+    return render(request, 'welcome.html')
