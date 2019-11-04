7de94bebe843348fcaa7eb54bba1ef172e2d28ee
nicky-code <aline.nicole7@gmail.com>
2019-11-04 08:52:53 +0200

    added the sign up.

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -5,6 +5,6 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^neighborhood/(?P<neighborhood_id>\d+)',views.neighborhood,name = 'neighborhood'),
      url(r'^profile/',views.new_profile,name = 'myProfile'),
-    url(r'^new/profile$', views.myprofile,name='profile'),
+    url(r'^new/profile$', views.new_profile,name='profile'),
      url(r'^newbusiness/',views.new_business,name = 'new-business'),
 ]
